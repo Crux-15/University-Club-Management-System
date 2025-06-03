b4c93712c12f0798151314d8100ced12cfe3df88
Acer <Acer@CRUX>
2025-06-04 02:42:54 +0600

    Add project files.

---
+++ Login.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Club_Management_System
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+
+        }
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void siticonePanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void siticonePanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void siticoneTextBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneTextBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneTextBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneTextBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneTextBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneTextBox11_Click(object sender, EventArgs e)
+        {
+
+
+
+
+
+
+        }
+
+        private void siticoneCheckBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneTextButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneTextButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneTextBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneTextBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneTextBox10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneTextBox9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneTextBox8_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneImageButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
